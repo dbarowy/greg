81a12941fe72acc879efc379202da6e1d933770e
Austin Osborn <ado2@williams.edu>
2023-11-16 14:27:33 -0500

    svg work

--- code/AST.fs
+++ code/AST.fs
@@ -1,18 +1,20 @@
 module AST
 
-type num = int
-type var = char
-type Color =
-| Red
-| Green
-| Blue
-| Purple
-type Function = 
-| Polynomial of Polynomial
-| Trig of Trig
-| Op of {first: Function; second: Function}
-type Polynomial = 
-| num
-| var
-type Plot = {f: Function; line: LineType; color: Color }
-type Graph = {plots: Plot list; domain: Domain}
+type Domain = {var: char; lower: int; upper: int}
+
+// type num = int
+// type var = char
+// type Color =
+// | Red
+// | Green
+// | Blue
+// | Purple
+// type Function = 
+// | Polynomial of Polynomial
+// | Trig of Trig
+// | Op of {first: Function; second: Function}
+// type Polynomial = 
+// | num
+// | var
+// type Plot = {f: Function; line: LineType; color: Color }
+// type Graph = {plots: Plot list; domain: Domain}
--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -1,3 +1,20 @@
 module Evaluator
 
 open AST
+
+// start of dynamic implimentation
+let rec evalCanvas (canvas: Canvas) : string =
+    match canvas with
+    | [] -> ""
+    | l::ls -> (evalLine l) + (evalCanvas ls)
+
+// all the nessesary static implimentation
+let eval (canvas: Canvas) : string =
+    let csz = CANVAS_SZ |> string
+    "<svg width=\"" + csz + "\" height=\"" + csz + "\"" +
+    " xmlns=\"http://www.w3.org/2000/svg\"" +
+    " xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n" +
+    " <style> .axisLabel {font: bold 20px sans-serif;} </style>\n" + 
+    " <rect x=\"10\" y=\"10\" rx=\"10\" ry=\"10\" width=\"" + csz + " height=\"" + csz + "\" style=\"fill:rgb(232,232,232);stroke-width:2;stroke:rgb(0,0,0)\"/>\n" +
+    // (evalCanvas canvas)
+    + "</svg>\n"
