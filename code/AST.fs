7a4164a2a8679d2c2926717c6f69e17803417034
danny klein <dak7@williams.edu>
2023-11-16 13:26:20 -0500

    ast

--- code/AST.fs
+++ code/AST.fs
@@ -1,1 +1,18 @@
 module AST
+
+type num = int
+type var = char
+type Color =
+| Red
+| Green
+| Blue
+| Purple
+type Function = 
+| Polynomial of Polynomial
+| Trig of Trig
+| Op of {first: Function; second: Function}
+type Polynomial = 
+| num
+| var
+type Plot = {f: Function; line: LineType; color: Color }
+type Graph = {plots: Plot list; domain: Domain}
