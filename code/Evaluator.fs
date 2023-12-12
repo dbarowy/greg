53f00a73d218414f4ecae318f07ff955c8f68ae1
danny klein <dak7@williams.edu>
2023-12-12 14:24:16 -0500

    examples

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -34,8 +34,8 @@ let rec draw_function (funct: float -> float, domain: Domain, cur_value: float, 
 
     let over_max = cur_value + (RESOLUTION/scaling_factor) >= domain.bounds.upper
 
-    let is_dash = ((iter % int(16.0/(RESOLUTION*2.0))) - 8) >= 0
-    let is_dot = (iter % int(9.0/(RESOLUTION*2.0))) = 0
+    let is_dash = ((iter % int(24.0/(RESOLUTION*2.0))) - 12) >= 0
+    let is_dot = (iter % int(16.0/(RESOLUTION*2.0))) = 0
 
     match line, over_max, is_dash, is_dot with 
     | _, true, _, _  -> ""
